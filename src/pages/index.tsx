e5df31548f3ae0bf2f7761b725d06ccef4f2897e
DaviLuisSantos <costasantos.daviluis@gmail.com>
2021-08-05 22:01:52 -0300

    por enquanto ta td tranks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,25 @@
-import Layout from "../components/layout";
+import Botao from "../components/Botao";
+import Layout from "../components/Layout";
+import Tabela from "../components/Tabela";
+import Cliente from "../core/Cliente";
 
 export default function Home() {
+
+  const clientes = [
+    new Cliente('Ana', 34, '1'),
+    new Cliente('Davi', 69, "6"),
+    new Cliente('Kid Bingo', 33, "3"),
+    new Cliente('sexoman', 57, '4')
+
+  ]
+
+  function clienteSelecionado(cliente: Cliente) {
+
+  }
+  function clienteExcluido(cliente: Cliente) {
+
+  }
+
   return (
     <div className={`
     flex justify-center items-center h-screen
@@ -8,7 +27,13 @@ export default function Home() {
     text-white
     `}>
       <Layout titulo="Cadastro Simples">
-        <span>Conteudo</span>
+        <div className='flex justify-end'>
+          <Botao cor="blue">Novo Cliente</Botao>
+        </div>
+        <Tabela clientes={clientes}
+          clienteSelecionado={clienteSelecionado}
+          clienteExcluido={clienteExcluido}
+        />
       </Layout>
     </div>
   )
